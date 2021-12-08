f5106f7ee89f50365f208730681626fb5692b159
mattdnkim <nyukcanada@gmail.com>
2021-12-07 17:33:28 -0700

    shortend URLs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = function(n) {
+  let randomString           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < n; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,9 +27,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -28,8 +40,42 @@ app.get("/urls", (req, res) => {
   console.log(templateVars);
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  // const longURL = req.body.longURL;
+  console.log(templateVars);
+  // res.redirect(longURL);
   res.render("urls_show", templateVars);
 });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL",(req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+// app.get("/example/:apple/:banana",(req ,res) => {
+//   console.log(req.params);
+//   res.send("hello");
+// });
+
+
